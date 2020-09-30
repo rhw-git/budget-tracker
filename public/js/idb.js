df8e3829d4357c55b9a878666d9dfe539314fd94
rhw <ruohanw89@gmail.com>
2020-09-29 21:44:40 -0600

    set up indexedDB offline saveRecord

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,19 +1,19 @@
 // create variable to hold db connection
 let db;
 // establish a connection to IndexedDB database called "budget_tracker"
-const request = indexedDB.open('budget_tracker', 1);
+let request = indexedDB.open('budget_tracker', 2);
 
 // this event will emit if the database version changes
 request.onupgradeneeded = function (event) {
   // save a reference to the database
-  const db = event.target.result;
+  db = event.target.result;
   // create an object store(table) called new_transaction and set it to have an auto incrementing primary key of sorts
-  db.createObjectStore('new_transcation', { autoIncrement: true });
+  db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 // upon a successful
 request.onsuccess = function (event) {
   // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
-  const db = event.target.result;
+  db = event.target.result;
   // check if app is online, if yes run uploadTranscation() function to send all local db data to api
   if (navigator.onLine) {
     //uploadTranscation();
@@ -27,11 +27,9 @@ request.onerror = function (event) {
 // when there is no internet connection
 function saveRecord(record) {
   // open a new transaction with the database with read and write permissions
-  console.log(db);
-  const transaction = db.transaction(['new_transaction'], 'readwrite');
-
+  let transaction = db.transaction(['new_transaction'], 'readwrite');
   // access the object store for "new_transaction"
-  const transactionObjectStore = transaction.ObjectStore('new_transaction');
+  let transactionObjectStore = transaction.objectStore('new_transaction');
   // add record to your store with add method
   transactionObjectStore.add(record);
 }
